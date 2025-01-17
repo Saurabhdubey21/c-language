eea7c098f9f27edc0dedbbcfa3ee5cf665152ddc
Saurabhdubey21 <mkd221524@gmail.com>
2025-01-17 22:11:45 +0530

    sum of integers and its square

---
+++ integerssquares.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int i,n,s,a;
+printf("n: ");
+scanf("%d", &n);
+for(i=1;i<=n;i++){
+    s=s+i;
+}
+    printf("sum: %d\n",s);
+      for(i=1;i<=s;i++){
+        a=s*s;
+    }
+        printf("sum of squares :%d\n",a);
+}
