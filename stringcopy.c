da44b1c07e3269333e898b798cb4427a366001a7
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-07 10:43:16 +0530

    string copy program in c

---
+++ stringcopy.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <string.h> 
+int main() { 
+char source[] = "Hello"; 
+    char destination[10]; 
+    strcpy(destination, source); 
+    printf("Copied String: %s\n", destination);  // Output: Hello 
+    return 0; 
+} 
