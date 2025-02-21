29183e7f914bccb8361e4aae947ea25d893a9b98
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-21 23:28:32 +0530

    passing string to function

---
+++ passingstringfunction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void printString(char str[]) { 
+    printf("String: %s\n", str); 
+    } 
+    int main() { 
+    char message[] = "Hello, Functions!"; 
+    printString(message); 
+    return 0; 
+    } 
