bb09627eb7e382eb384f36681d316635d257c6ac
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-24 23:27:38 +0530

    find string lengthby using strlen() function

---
+++ stringlength1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() { 
+    char str[100]; 
+    int count = 0; 
+    printf("Enter a string: "); 
+    scanf("%s", str); 
+    while (str[count] != '\0') { 
+        count++; 
+    } 
+    printf("Length of the string: %d\n", count); 
+    return 0; 
+}
